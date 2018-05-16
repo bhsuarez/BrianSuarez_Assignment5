939cbb93ed796039900e95fc76d13a251ddf7089
bhsuarezz <-Lgh]DaUYTJrF0NL>
2018-05-16 12:37:08 -0500

    Created Planetvisit model and controller

--- src/main/java/com/bhsuarez/week5/DataStore.java
+++ src/main/java/com/bhsuarez/week5/DataStore.java
@@ -1,13 +1,12 @@
 package com.bhsuarez.week5;
 
 import java.util.List;
-import java.util.Iterator;
 
 import com.bhsuarez.week5.models.Planet;
+import com.bhsuarez.week5.models.Planetvisit;
 import com.bhsuarez.week5.models.Starship;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.HibernateException;
 
@@ -17,15 +16,6 @@ public class DataStore {
 
     private static SessionFactory buildSessionFactory() {
         try {
-            /*
-            String dbName = System.getenv("RDS_DB_NAME");
-            String userName = System.getenv("RDS_USERNAME");
-            String password = System.getenv("RDS_PASSWORD");
-            String hostname = System.getenv("RDS_HOSTNAME");
-            String port = System.getenv("RDS_PORT");
-            String jdbcUrl = "jdbc:oracle:thin:@" + hostname + ":" + port + ":" + dbName;
-            */
-            System.out.println("test");
 
             // Create the SessionFactory from hibernate.cfg.xml
             Configuration cfg = new Configuration();
@@ -48,15 +38,7 @@ public class DataStore {
 
 
         try {
-
             List planets = session.createQuery("FROM Planet").list();
-            for (Iterator iterator = planets.iterator(); iterator.hasNext(); ) {
-                Planet planet = (Planet) iterator.next();
-                System.out.print("Name: " + planet.getPlanetName());
-                System.out.print("  Radius: " + planet.getPlanetRadius());
-                System.out.print("  Atmosphere: " + planet.getPlanetAtmosphere());
-            }
-
             return planets;
         } catch (HibernateException e) {
             e.printStackTrace();
@@ -72,16 +54,24 @@ public class DataStore {
         Session session = getSessionFactory().openSession();
 
         try {
-
             List starships = session.createQuery("FROM Starship").list();
-            for (Iterator iterator = starships.iterator(); iterator.hasNext(); ) {
-                Starship starship = (Starship) iterator.next();
-                System.out.print("Name: " + starship.getStarshipName());
-                System.out.print("  Crew Size: " + starship.getStarshipCrewSize());
-                System.out.print("  Ship Class: " + starship.getStarshipClass());
-            }
-
             return starships;
+        } catch (HibernateException e) {
+            e.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return null;
+    }
+
+    public static List<Planetvisit> listPlanetVisits( ) {
+        System.out.print("listPlanetVisits()");
+
+        Session session = getSessionFactory().openSession();
+
+        try {
+            List planetvisits = session.createQuery("FROM Planetvisit").list();
+            return planetvisits;
         } catch (HibernateException e) {
             e.printStackTrace();
         } finally {
--- src/main/java/com/bhsuarez/week5/PlanetVisitController.java
+++ src/main/java/com/bhsuarez/week5/PlanetVisitController.java
@@ -1,4 +1,15 @@
 package com.bhsuarez.week5;
 
+import com.bhsuarez.week5.models.Planetvisit;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class PlanetVisitController {
+
+    @RequestMapping("/planetvisit")
+    public List<Planetvisit> planetVisits() {
+        return DataStore.listPlanetVisits();
+    }
 }
